cf934e66fdfcb2601f4050a595db4a6070a612db
Ramiz Raja <informramiz@gmail.com>
2020-03-16 03:23:48 +0500

    feat: Define custom view cell

--- NASA-NeoWs/NASA-NeoWs/Controller/FeedViewController.swift
+++ NASA-NeoWs/NASA-NeoWs/Controller/FeedViewController.swift
@@ -2,14 +2,15 @@
 //  ViewController.swift
 //  NASA-NeoWs
 //
-//  Created by Apple on 15/03/2020.
+//  Created by Ramiz on 15/03/2020.
 //  Copyright © 2020 RR Inc. All rights reserved.
 //
 
 import UIKit
 
 class FeedViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
---
+++ NASA-NeoWs/NASA-NeoWs/View/CustomViews/FeedTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  FeedTableViewCell.swift
+//  NASA-NeoWs
+//
+//  Created by Apple on 16/03/2020.
+//  Copyright © 2020 RR Inc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FeedTableViewCell: UITableViewCell {
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var subtitle: UILabel!
+    
+}
