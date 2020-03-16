9175ff25f64b772b269f7a558c5de94438f9e932
Ramiz Raja <informramiz@gmail.com>
2020-03-16 22:53:32 +0500

    feat: Connect tableview with feed

--- NASA-NeoWs/NASA-NeoWs/Controller/FeedViewController.swift
+++ NASA-NeoWs/NASA-NeoWs/Controller/FeedViewController.swift
@@ -8,21 +8,56 @@
 
 import UIKit
 
-class FeedViewController: UIViewController {
+class FeedViewController: UIViewController, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
+    var asteriods: Asteriods? = nil
+    var sectionIndexToDateMapping: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         loadFeed()
+        tableView.dataSource = self
     }
 
     private func loadFeed() {
         NASAAPIClient.getAsteriodFeed(successHandler: { asteriods in
-            print(asteriods)
+            self.asteriods = asteriods
+            self.mapSections()
+            self.tableView.reloadData()
         }) { error in
-            print(error)
+            self.showErrorAlert(message: error.localizedDescription)
         }
+    }
+    
+    private func mapSections() {
+        guard let asteriodsList = self.asteriods?.nearEarthObjects else {
+            sectionIndexToDateMapping = []
+            return
+        }
+        
+        sectionIndexToDateMapping = []
+        for asteriod in asteriodsList {
+            sectionIndexToDateMapping.append(asteriod.key)
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let asteriodsList = self.asteriods?.nearEarthObjects else {
+            return 0
+        }
+        
+        return asteriodsList[sectionIndexToDateMapping[section]]?.count ?? 0
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return asteriods?.nearEarthObjects.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let tableViewCell = tableView.dequeueReusableCell(withIdentifier: "FeedTableViewCell") as! FeedTableViewCell
+        
+        return tableViewCell
     }
 }
 
